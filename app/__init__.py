f9b500af412085298b3d6b6cd561f94bbdd67493
FMM28 <marquez.maya.francisco.28@gmail.com>
2026-02-17 13:08:25 -0600

    implementacion csrf

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from dotenv import load_dotenv
 from .config import Config
-from .extensions import db, migrate, bcrypt, login_manager, socketio
+from .extensions import db, migrate, bcrypt, login_manager, socketio, csrf
 from app.auth.login_manager import load_user
 from .routes import register_blueprints
 import app.sockets
@@ -19,6 +19,7 @@ def create_app():
     migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    csrf.init_app(app)
 
     @app.teardown_appcontext
     def shutdown_session(exception=None):
